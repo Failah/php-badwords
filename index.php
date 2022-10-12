ec7b592d3237c333f5fa75ddbeb8ed8ac06cde41
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-12 13:50:07 +0200

    Finish

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 
 <?php
 
-$text = 'Ciao sono una variabile con un paragrafo di testo a mia scelta!'
+$text = 'Ciao sono una variabile con un paragrafo di testo a mia scelta!';
+
+$censoredWord = $_GET['word'];
 
 ?>
 
@@ -13,5 +15,11 @@ $text = 'Ciao sono una variabile con un paragrafo di testo a mia scelta!'
     <?= $text ?>
 </h1>
 <p>
-    <?= 'Lunghezza della variabile: ' . '' ?>
+    <?= 'Lunghezza della variabile: ' . strlen($text) ?>
+</p>
+<h1>
+    <?= 'Variabile di testo con parola censurata: ' . str_replace($censoredWord, '***', $text) ?>
+</h1>
+<p>
+    Inserisci nell'URL ---> "?word=PAROLA" sostituendo PAROLA con una da dover censurare nella variabile di testo
 </p>
